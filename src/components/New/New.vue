95a35f30a26b4485be6f06a7f748c86e78e8b300
陈嘉伟 <1300549908@qq.com>
2019-04-10 16:02:47 +0800

    消息组件制作

--- src/components/New/New.vue
+++ src/components/New/New.vue
@@ -1,18 +1,75 @@
 <template>
-    <div>
-      消息
-    </div>
+  <div>
+    <mu-paper>
+      <mu-container ref="container">
+        <mu-load-more>
+          <mu-list>
+            <template v-for="i in num" >
+              <!--eslint-disable-next-line-->
+              <mu-list-item>
+                <div style="width: 100%;">
+                  <div style="display: inline-block;padding-top: 5px;">
+                    <mu-avatar size="45">
+                      <img src="../../assets/logo.png">
+                    </mu-avatar>
+                  </div>
+                  <div style="display: inline-block;margin-left: 10px; vertical-align:top;height: 55px;">
+                    <span style="display: block;margin-top: 2px;">标题</span>
+                    <span style="display:block;margin-top: 5px;">内容: ...{{i}}</span>
+                  </div>
+                  <div style="display: inline-block;float: right;height: 55px;">
+                    <span style="margin-top: 10px;display: block">2019-04-18</span>
+                    <span style="float: right">
+                      <mt-badge size="small" type="error" style="border-radius: 20px;">10</mt-badge>
+                    </span>
+                  </div>
+                </div>
+              </mu-list-item>
+              <!--eslint-disable-next-line-->
+              <mu-divider />
+            </template>
+          </mu-list>
+        </mu-load-more>
+      </mu-container>
+    </mu-paper>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'New',
-  created() {
-    console.log('消息')
+  data () {
+    return {
+      num: 10,
+      refreshing: false,
+      loading: false,
+      text: 'List'
+    }
+  },
+  methods: {
+    refresh () {
+      this.refreshing = true
+      this.$refs.container.scrollTop = 0
+      setTimeout(() => {
+        this.refreshing = false
+        this.text = this.text === 'List' ? 'Menu' : 'List'
+        this.num = 10
+      }, 2000)
+    },
+    load () {
+      this.loading = true
+      setTimeout(() => {
+        this.loading = false
+        this.num += 10
+      }, 2000)
+    }
   }
 }
 </script>
 
-<style scoped>
-
+<style>
+.mu-item{
+  height: auto;
+  padding: 0px 10px;
+}
 </style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -2,47 +2,46 @@
   <div style="width: 100%;height: 100%">
     <div style="width: 100%;height: 100%">
       <div ref="header" class="header">
-        <mt-header title="消息">
+        <mt-header :title="selected">
           <router-link to="/" slot="left">
             <mt-button icon="back"></mt-button>
           </router-link>
         </mt-header>
       </div>
-      <div class="container" :style="{ 'height': containerHeight + 'px' }">
+      <div id="scroll" class="index_container" :style="{ 'height': containerHeight + 'px' }">
         <!-- tab-container -->
-        <mt-tab-container v-model="selected">
-          {{selected}}
-          <mt-tab-container-item id="1">
-            <new v-if="selected == '1'" />
+        <mt-tab-container  v-model="selected">
+          <mt-tab-container-item :id="title[0]">
+            <new v-if="selected == title[0]" />
           </mt-tab-container-item>
-          <mt-tab-container-item id="2">
-            <relation-people v-if="selected == '2'" />
+          <mt-tab-container-item :id=title[1]>
+            <relation-people v-if="selected == title[1]" />
           </mt-tab-container-item>
-          <mt-tab-container-item id="3">
-            <explore v-if="selected == '3'"/>
+          <mt-tab-container-item :id=title[2]>
+            <explore v-if="selected == title[2]"/>
           </mt-tab-container-item>
-          <mt-tab-container-item id="4">
-            <my-center v-if="selected == '4'" />
+          <mt-tab-container-item :id="title[3]">
+            <my-center v-if="selected == title[3]" />
           </mt-tab-container-item>
         </mt-tab-container>
       </div>
       <div ref="bottom" class="bottom">
         <mt-tabbar v-model="selected">
-          <mt-tab-item id="1">
+          <mt-tab-item :id="title[0]">
             <img slot="icon" src="../assets/logo.png">
-            消息
+            {{title[0]}}
           </mt-tab-item>
-          <mt-tab-item id="2">
+          <mt-tab-item :id="title[1]">
             <img slot="icon" src="../assets/logo.png">
-            联系人
+            {{title[1]}}
           </mt-tab-item>
-          <mt-tab-item id="3">
+          <mt-tab-item :id="title[2]">
             <img slot="icon" src="../assets/logo.png">
-            发现
+            {{title[2]}}
           </mt-tab-item>
-          <mt-tab-item id="4">
+          <mt-tab-item :id="title[3]">
             <img slot="icon" src="../assets/logo.png">
-            我
+            {{title[3]}}
           </mt-tab-item>
         </mt-tabbar>
       </div>
@@ -55,6 +54,8 @@ import New from '@/components/New/New.vue'
 import RelationPeople from '@/components/RelationPeople/RelationPeople.vue'
 import Explore from '@/components/Explore/Explore.vue'
 import MyCenter from '@/components/MyCenter/MyCenter.vue'
+import BScroll from 'better-scroll'
+
 export default {
   name: 'index',
   components: {
@@ -65,22 +66,34 @@ export default {
   },
   data () {
     return {
-      selected: '1',
+      title: ['消息', '联系人', '发现', '我'],
+      selected: '消息',
       clientHeight: document.documentElement.clientHeight, // 页面高度
-      containerHeight: 0
-    }
-  },
-  watch: {
-    clientHeight() {
-      this.computeHeight()
+      containerHeight: 0,
+      scroll: null
     }
   },
   mounted() {
     this.computeHeight()
+    this.initBScroll()
   },
   methods: {
+    initBScroll() {
+      // 初始化BScroll
+      if (this.scroll == null) {
+        this.scroll = new BScroll('#scroll', {
+          scrollY: true,
+          click: true
+        })
+      }
+      this.scroll.on('touchend', (pos) => {
+        console.log(pos)
+        if (pos.y > 50) {
+          console.log('下拉加载......')
+        }
+      })
+    },
     computeHeight() {
-      console.log(111)
       let headerHeight = this.$refs.header.children[0].clientHeight
       let bottomHeight = this.$refs.bottom.children[0].clientHeight
       this.containerHeight = this.clientHeight - headerHeight - bottomHeight
@@ -90,7 +103,8 @@ export default {
 </script>
 
 <style scoped>
-.header{
-
+.index_container{
+  overflow: hidden;
+  background: white;
 }
 </style>
